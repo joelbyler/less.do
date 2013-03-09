0166b88c100df3ca3e127d421e9f2814c1126b3e
Joel Byler <joelbyler@gmail.com>
2013-03-09 16:00:53 -0500

    Sample json data and jquery templ for display of categories

--- Less/war/js/main.js
+++ Less/war/js/main.js
@@ -1,27 +1,82 @@
 var base_url = "http://localhost:8888/";
+var CATEGORY_NAME_MAIN = "top_level";
+var url = base_url + "/data/sample.json";
+var TEMPLATE_SUB_CATEGORY = "<span style=\"width: 50px;\"></span><a href=\"#\" onclick=\"displayProducts('${name}');\" title=\"${label}\"><img src=\"${img_url}\" class=\"img-circle\"></a>";
+var TEMPLATE_PRODUCT = 	"<div class=\"row\">" +
+"<hr><div class=\"span8\">" +
+"    <div class=\"row\">" +
+"      <div class=\"span8\">" +
+"        <h4><strong><a href=\"#\">${label}</a></strong></h4>" +
+"      </div>" +
+"    </div>" +
+"    <div class=\"row\">" +
+"      <div class=\"span2\">" +
+"        <a href=\"#\" class=\"thumbnail\">" +
+"          <img src=\"${img_url}\" alt=\"{label}\">" +
+"        </a>" +
+"      </div>" +
+"      <div class=\"span6\">      " +
+"        <p>${description}</p>" +
+"      </div>" +
+"    </div>" +
+"    <div class=\"row\">" +
+"      <div class=\"span8\">" +
+"        <p></p>" +
+"        <p>" +
+"          <i class=\"icon-comment\"></i> <a href=\"#\">${comments} Comments</a> | " +
+"          <i class=\"icon-tags\"></i> Tags : " +
+"			{{each tags}} "+
+"		 		<a href=\"#\"><span class=\"label label-info\">${$value}</span></a>" + 
+"			{{/each}}  " +
+"        </p>" +
+"      </div>" +
+"    </div>" +
+"  </div>" +
+"</div>";
 
-function displayMainComponents() {
-	var url = base_url + "component";
-	var stringToAppend = '';
+
+$.template( "subCategoryCategory", TEMPLATE_SUB_CATEGORY );
+$.template( "productCategory", TEMPLATE_PRODUCT );
+
+function loadMainCategory() {
 	$.getJSON(url, function(data) {
-		if (data.status === "OK") {
-			$.each(JSON.parse(data.response).allComponents, function(key, value) {
-				stringToAppend+='<div class="row"><div class="span8"><div class="row"><div class="span8"><h4><strong><a href="#">'+value.name+'</a></strong></h4></div></div><div class="row"><div class="span2"><a href="#" class="thumbnail"><img src="'+value.imageUrl+'" height="180" width="260" alt="'+value.name+'"></a></div><div class="span6"><p>'+value.description+'</p><p><a class="btn" href="'+base_url+'component_detail.html?id='+value.key.name+'">Details</a></p></div></div><div class="row"><div class="span8"><p></p><p><i class="icon-user"></i> por <a href="#">'+value.author+'</a>  | <i class="icon-calendar"></i> //TODO DATE Sept 16th, 2012        | <i class="icon-comment"></i> <a href="#">0 Comments</a>| <i class="icon-share"></i> <a href="#">Share</a> | <i class="icon-tags"></i> Tags :'; 
-					$.each(value.tags, function(key, value) {
-						stringToAppend+=' <a href="#"><span class="label label-info">'+value+'</span></a>';
-					});
-					stringToAppend+='</p></div></div></div>	</div><hr>';				
-			});
-		}
-		$("#main_components").append(stringToAppend);
+		var select = $("#selectTopLevelCategory");
+		select.find('option').remove().end();
+		select.append($('<option>').text("Select One"));
+		$.each(data.main_categories, function(key, value) {
+			if (value.tags.indexOf(CATEGORY_NAME_MAIN) >=0){
+				select.append($('<option>').text(value.label).attr('value', value.name));
+			}
+		});
 	});
 }
 
-
-function displaySuggestedCategories() {
-	$("#main_suggested_categories").show();
+function displaySubCategories(forTag) {
+	var url = base_url + "/data/sample.json";
+	$.getJSON(url, function(data) {
+		$("#main_suggested_categories_list").html("");
+		var hitCounter=0;
+		$.each(data.sub_categories, function(key, value) {
+			if (value.tags.indexOf(forTag) >=0){
+			    $.tmpl( "subCategoryCategory", value ).appendTo( "#main_suggested_categories_list" );
+				hitCounter++;
+			}
+		});
+		$("#suggested_category_item_count").html("("+hitCounter+")");
+		$("#main_suggested_categories").show();
+	});
 }
 
-function displaySuggestedElements() {
-	$("#main_suggested_elements").show();
+function displayProducts(forTag) {
+	var url = base_url + "/data/sample.json";
+	$.getJSON(url, function(data) {
+		$("#main_suggested_elements").html("");
+		$.each(data.products, function(key, value) {
+			if (value.tags.indexOf(forTag) >=0){
+			    $.tmpl( "productCategory", value ).appendTo( "#main_suggested_elements" );
+			}
+		});
+		$("#main_suggested_elements").show();
+	});
 }
+
